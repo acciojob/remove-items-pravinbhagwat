3dffbdbd8a10ec1cfb373734099d8edcfd38e749
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-23 19:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the select element
+const colorSelect = document.getElementById('colorSelect');
+
+// Get the remove button element
+const removeButton = document.querySelector('input[type="button"]');
+
+// Add a click event listener to the remove button
+removeButton.addEventListener('click', () => {
+  // Get the selected option element
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+  // Remove the selected option element
+  colorSelect.removeChild(selectedOption);
+});
